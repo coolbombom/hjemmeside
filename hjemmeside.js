da8109584b467b46ea302efc47807c44ace7617a
coolbombom <cool_bombom@hotmail.com>
2016-02-07 18:59:34 +0100

    Update hjemmeside.js

--- hjemmeside.js
+++ hjemmeside.js
@@ -31,7 +31,7 @@ function addEvent(elem, event, fn) {
 
 function setGalleryBoxStyle(){
     var elem = document.querySelector(".gallery-box")
-    if ( ! elem.className.match(/(?:^|\s)MyClass(?!\S)/) ) elem.className = elem.className + "visible";
+    if ( ! elem.className.match(/(?:^|\s)MyClass(?!\S)/) ) elem.className = elem.className + " visible";
     elem = elem.querySelector(".helper-div");
     if (window.innerWidth > 1024) {
        elem.className = elem.className.replace( /(?:^|\s)middle-center(?!\S)/g , '' ) + " middle-left";
