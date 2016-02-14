82d2448f720408c1b6dbc83ef09833e081517586
coolbombom <cool_bombom@hotmail.com>
2016-02-14 11:50:07 +0100

    Update hjemmeside.js

--- hjemmeside.js
+++ hjemmeside.js
@@ -48,7 +48,10 @@ window.onload = function(){
      setGalleryBoxStyle();
 }
 
-console.debug(document);
-console.debug(window);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-window.parent.document.getElementsByTagName('head')[0].innerHTML += '<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" name="viewport">';
+ga('create', 'UA-73770324-1', 'auto');
+ga('send', 'pageview');
