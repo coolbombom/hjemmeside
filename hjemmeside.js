51c685694bb3b8dac4f7cb54578b512bbd7d5cfb
coolbombom <cool_bombom@hotmail.com>
2017-02-04 14:45:50 +0100

    Update hjemmeside.js

--- hjemmeside.js
+++ hjemmeside.js
@@ -42,6 +42,7 @@ function setGalleryBoxStyle(){
 }
 
 $(document).ready(function(){
+     alert("rwwfsf");
      document.getElementById("PINTEREST").querySelector("img").src="http://www.ccs.neu.edu/home/amirali/include/black-white-metro-github-icon.png";
 
      addEvent(window, 'resize', setGalleryBoxStyle);
